a570e1fa4e5a965e37a0b8e9443b0b10f4883cc1
t00n <kelkununtel@hotmail.com>
2013-11-05 13:36:50 +0100

    Param loop

--- generateFile.py
+++ generateFile.py
@@ -1,1 +1,26 @@
 #!/bin/python3
+
+import sys
+
+
+args=sys.argv
+extension=args[1]
+main=args[2] 
+
+classOption=False
+fileOption=False
+classList=[]
+fileList=[]
+
+for param in args:
+    if param == "-c":
+        classOption=True
+        fileOption=False
+    elif param == "-f":
+        classOption=False
+        fileOption=True
+    elif classOption:
+        classList.append(param)
+    elif fileOption:
+        fileList.append(param)
+

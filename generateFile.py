f04340676ecd3b2324da51e9cfa2fb908e840b87
etnarek <romain-fontaine@outlook.com>
2013-11-05 13:37:09 +0100

    add main fonction

--- generateFile.py
+++ generateFile.py
@@ -1,27 +1,29 @@
 #!/bin/python3
 import sys
 
-cppTemplate={"main":"cppMainTemplate","make":"makeFileTemplate", "class":["cppClassTemplate", "hppClassTemplate"], "file":["cppTemplate", "hppTemplate"]}
-cTemplate={"main":"cMainTemplate","make":"makeFileTemplate", "file":["cTemplate", "hTemplate"]}
+def main():
+    cppTemplate={"main":"cppMainTemplate","make":"makeFileTemplate", "class":["cppClassTemplate", "hppClassTemplate"], "file":["cppTemplate", "hppTemplate"]}
+    cTemplate={"main":"cMainTemplate","make":"makeFileTemplate", "file":["cTemplate", "hTemplate"]}
 
-args=sys.argv
-extension=args[1]
-main=args[2]
+    args=sys.argv
+    extension=args[1]
+    mainFile=args[2]
 
-classOption=False
-fileOption=False
-classList=[]
-fileList=[]
+    classOption=False
+    fileOption=False
+    classList=[]
+    fileList=[]
 
-for param in args:
-    if param == "-c":
-        classOption=True
-        fileOption=False
-    elif param == "-f":
-        classOption=False
-        fileOption=True
-    elif classOption:
-        classList.append(param)
-    elif fileOption:
-        fileList.append(param)
+    for param in args:
+        if param == "-c":
+            classOption=True
+            fileOption=False
+        elif param == "-f":
+            classOption=False
+            fileOption=True
+        elif classOption:
+            classList.append(param)
+        elif fileOption:
+            fileList.append(param)
 
+main()

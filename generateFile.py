e6fb79c48d12ab42d61685edbfd0f2bf86c67283
etnarek <romain-fontaine@outlook.com>
2013-11-05 12:30:41 +0100

    Added dico of Template for c and cpp

--- generateFile.py
+++ generateFile.py
@@ -1,1 +1,4 @@
 #!/bin/python3
+
+cppTemplate={"main":"cppMainTemplate","make":"makeFileTemplate", "class":["cppClassTemplate", "hppClassTemplate"], "file":["cppTemplate", "hppTemplate"]}
+cTemplate={"main":"cMainTemplate","make":"makeFileTemplate", "file":["cTemplate", "hTemplate"]}
